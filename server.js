e3ec37422fdd58ecdbdf3d529bbc5613b51dd26b
ASDT000 <asdinushkatharidu@gmail.com>
2025-10-03 19:00:40 +0530

    add cors for server

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 import express from 'express';
 import dotenv from "dotenv";
-import todoRoutes from "./routes/todo.route.js"
+import cors from "cors";
+import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from './config/db.js';
 
 dotenv.config();
 
-
 const app = express();
+
+// CORS setup: allow your Netlify frontend domain
+app.use(cors({
+  origin: "https://simple-mern-todo-app-frontend.netlify.app"
+}));
 
 app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
 
 app.listen(5000, () => {
-
-    connectDB();
-    console.log("server started  at the http://localhost:5000");
-
-    
-})
+  connectDB();
+  console.log("server started at http://localhost:5000");
+});
